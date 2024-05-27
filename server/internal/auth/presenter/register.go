e11069d51514bbc09671ee4c5069c2de8dd04e8c
thanhhuy25 <nguyenhuythanh256@gmail.com>
2024-05-27 17:15:54 +0700

    Hoàn thàng chức năng đăng nhập với email, password và trả về JWT

--- server/internal/auth/presenter/register.go
+++ server/internal/auth/presenter/register.go
@@ -4,12 +4,9 @@ type RegisterPayload struct {
 	Email           string `json:"email" validate:"required,email"`
 	Password        string `json:"password" validate:"required,min=8,max=32,printascii"`
 	ConfirmPassword string `json:"confirm_password" validate:"required,min=8,max=32,printascii,eqfield=Password"`
-	Name string `json:"name" validate:"required,min=3,max=50"`
-	
-
+	Name            string `json:"name" validate:"required,min=3,max=50"`
 }
 
 type RegisterOutput struct {
 	Message string `json:"message"`
-
 }
--- server/internal/auth/usecase/usecase.go
+++ server/internal/auth/usecase/usecase.go
@@ -3,12 +3,12 @@ package usecase
 import (
 	"context"
 	"fmt"
+	"spotify/config"
 	"spotify/internal/auth"
 	"spotify/internal/models"
 	"spotify/internal/security"
 	"spotify/utils"
 )
-
 
 type AuthUseCase struct {
 	authRepo auth.UserRepo
@@ -19,7 +19,7 @@ func NewAuthUseCase(authRepo auth.UserRepo) *AuthUseCase {
 }
 
 func (a *AuthUseCase) Register(ctx context.Context, email, password, conformPassword, name string) error {
-	_, err := a.authRepo.GetAccountByEmail(ctx,email)
+	_, err := a.authRepo.GetAccountByEmail(ctx, email)
 	if err == nil {
 		return fmt.Errorf(auth.ErrUserExisted.Error())
 	}
@@ -32,21 +32,36 @@ func (a *AuthUseCase) Register(ctx context.Context, email, password, conformPass
 	if err != nil {
 		return fmt.Errorf(auth.ErrInternalServer.Error())
 	}
-	
-	account := &models.Account{Email:email, Password:hashPassword}
+
+	account := &models.Account{Email: email, Password: hashPassword}
 	if err := a.authRepo.CreateAccount(ctx, account); err != nil {
 		return fmt.Errorf(auth.ErrInternalServer.Error())
 	}
 
-
-	if err := a.authRepo.CreateUser(ctx, &models.Users{Name:name, AccountID:account.ID, Slug: utils.CreateSlug(name, account.ID)}); err != nil {
+	if err := a.authRepo.CreateUser(ctx, &models.Users{Name: name, AccountID: account.ID, Slug: utils.CreateSlug(name, account.ID)}); err != nil {
 		return fmt.Errorf(auth.ErrInternalServer.Error())
 	}
 
 	return nil
 }
 
-
 func (a *AuthUseCase) Login(ctx context.Context, email, password string) (token string, err error) {
-	return "", nil
+
+	accout, err := a.authRepo.GetAccountByEmail(ctx, email)
+	if err != nil {
+		return "", fmt.Errorf(auth.ErrUserNotFound.Error())
+	}
+
+	if !security.ComparePassword(accout.Password, []byte(password)) {
+		return "", fmt.Errorf(auth.ErrWrongPassword.Error())
+	}
+
+	secret := []byte(config.Envs.JWRSecret)
+	token, err = security.CreateJWT(secret, int(accout.ID))
+
+	if err != nil {
+		return "", fmt.Errorf(auth.ErrInternalServer.Error())
+	}
+
+	return token, nil
 }
