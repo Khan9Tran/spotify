be2fc0acb39a27a9d44ec578de8e657c414a78c2
thanhhuy25 <nguyenhuythanh256@gmail.com>
2024-05-27 23:25:42 +0700

    Thêm tường role cho bảng User

--- server/internal/models/users.go
+++ server/internal/models/users.go
@@ -1,19 +1,20 @@
 package models
 
 import (
-    "gorm.io/gorm"
-    "time"
+	"gorm.io/gorm"
+	"time"
 )
 
 type Users struct {
-    gorm.Model
-    Name        string `gorm:"type:varchar(100);not null"`
-    Slug        string `gorm:"type:varchar(100);not null;unique"`
-    Country     string `gorm:"type:varchar(100)"`
-    DateOfBirth time.Time
-    Gender      string `gorm:"type:varchar(10)"`
-    AccountID   uint `gorm:"not null"`
-    Account Account `gorm:"foreignKey:AccountID;references:ID"`
-    Artists     []Artist `gorm:"many2many:follows"`
-    Song       []Song `gorm:"many2many:listening_histories"`
+	gorm.Model
+	Name        string `gorm:"type:varchar(100);not null"`
+	Slug        string `gorm:"type:varchar(100);not null;unique"`
+	Country     string `gorm:"type:varchar(100)"`
+	DateOfBirth time.Time
+	Gender      string   `gorm:"type:varchar(10)"`
+	AccountID   uint     `gorm:"not null"`
+	Role        string   `gorm:"type:varchar(10);not null"`
+	Account     Account  `gorm:"foreignKey:AccountID;references:ID"`
+	Artists     []Artist `gorm:"many2many:follows"`
+	Song        []Song   `gorm:"many2many:listening_histories"`
 }
