36cb77b72c3dc3ae08461d61fcdfc720f7a61416
Khan9Tran <trankhangtctv@gmail.com>
2024-05-28 20:20:55 +0700

    fix

--- server/internal/auth/repository.go
+++ server/internal/auth/repository.go
@@ -9,3 +9,7 @@ type AccountRepo interface {
 	CreateAccount(ctx context.Context, user *models.Account) error
 	GetAccountByEmail(ctx context.Context, email string) (*models.Account, error)
 }
+
+type UserRepo interface {
+	CreateUser(ctx context.Context, user *models.Users) error
+}
