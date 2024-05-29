89791ee5e89405eedf98de265cf1f16014167ce8
thanhhuy25 <nguyenhuythanh256@gmail.com>
2024-05-29 23:50:31 +0700

    Cập nhật màu sắc custom của trang web.

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,29 +5,28 @@ module.exports = {
   ],
   theme: {
     extend: {
-      extend: {
         colors: {
-          'bg-black-primary': '#000000',
-          'bg-black-secondary': '#121212',
+          'black-primary': '#000000',
+          'black-secondary': '#121212',
 
-          'txt-white-primary': '#ffffff',
-          'txt-white-secondary': '#f6f6f6',
+          'white-primary': '#ffffff',
+          'white-secondary': '#f6f6f6',
 
-          'bg-green-primary': '#1fdf64',
-          'txt-green-primary': '#117a37',
+          'green-light': '#1fdf64',
+          'green-dark': '#117a37',
 
-          'bg-gray-primary': '#727272',
-          'txt-gray-primary': '#a7a7a7',
+          'gray-light': '#a7a7a7',
+          'gray-dark': '#727272',
 
-          'bg-red-primary': '#e91429',
-          'txt-red-primary': '#e35e60',
+          'red-light': '#e91429',
+          'red-dark': '#e35e60',
         },
+
         fontFamily: {
           'bold': ['CircularStd-Bold', 'sans-serif'],
           'light': ['CircularStd-Light', 'sans-serif'],
           'medium': ['CircularStd-Medium', 'sans-serif'],
         },
-      },
     },
   },
   plugins: [],
