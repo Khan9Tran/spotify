c32549d777024bed85e54c2e334d599b5e500e03
thanhhuy25 <nguyenhuythanh256@gmail.com>
2024-05-31 01:53:26 +0700

    Tạo giao diên trang login (chưa hoàn thiện).

--- client/src/pages/login/login.jsx
+++ client/src/pages/login/login.jsx
@@ -1,8 +1,87 @@
 import React from 'react'
 import './login.css'
+import Logo from '../../components/logo'
+import Input from '../../components/input'
+import Button from '../../components/button'
+import GoogleLogo from '../../assets/images/google_logo.png'
+import FacebookLogo from '../../assets/images/facebook_logo.png'
+import AppleLogo from '../../assets/images/apple_logo.png'
 
 export const Login = () => {
   return (
-    <div>login</div>
+    <div className="wrapper bg-black-secondary w-screen h-screen flex flex-col">
+      <div className="wrapper--header w-full h-[96px] flex items-center pl-7">
+        <Logo />
+      </div>
+
+      <div className="wrapper--body flex flex-col items-center ">
+        <div className="w-[734px] px-7 flex flex-col items-center">
+          <h1 className="text-white-primary text-[32px] sm:text-[32px] font-bold my-12">
+            Đăng nhập vào Spotify
+          </h1>
+
+          <div className='w-full sm:w-[324px]'>
+            <div className='mb-3'>
+              <a className='w-full h-[48px] rounded-[60px] border-gray-dark hover:border-white-primary hover:border-[1.9px] border-[1.5px] px-6 flex flex-row items-center justify-between' href="#">
+                <img src={GoogleLogo} alt="" />
+                <span className='font-bold text-[15px]'>Tiếp tục bằng Google</span>
+                <div className='w-5'></div>
+              </a>
+            </div>
+            <div className='mb-3'>
+              <a className='w-full h-[48px] rounded-[60px] border-gray-dark hover:border-white-primary hover:border-[1.9px] border-[1.5px] px-6 flex flex-row items-center justify-between' href="#">
+                <img src={FacebookLogo} alt="" />
+                <span className='font-bold text-[15px]'>Tiếp tục bằng Facebook</span>
+                <div className='w-5'></div>
+              </a>
+            </div>
+            <div className='mb-3'>
+              <a className='w-full h-[48px] rounded-[60px] border-gray-dark hover:border-white-primary hover:border-[1.9px] border-[1.5px] px-6 flex flex-row items-center justify-between' href="#">
+                <img src={AppleLogo} alt="" />
+                <span className='font-bold text-[15px]'>Tiếp tục bằng Apple</span>
+                <div className='w-5'></div>
+              </a>
+            </div>
+          </div>
+
+          <div className='w-3/4 h-[1px] my-11 bg-gray-dark'></div>
+
+          <form className="w-full sm:w-[324px]">
+            <label className='block font-bold'>Email hoặc tên người dùng</label>
+            <div className='mt-2'>
+              <Input type={'email'} placeholder={'Email hoặc tên người dùng'} width={'100%'} height={'48px'} radius={'5px'} padding={'0 10px'} fontSize={'15px'} borderWidth={'1.5px'} changeBorderColor={true} borderColor={'border-gray-dark'} borderColorForcused={'border-white-primary'} borderColorError={'border-red-light'} />
+            </div>
+            <label className='block font-bold mt-3'>Mật khẩu</label>
+            <div className='my-2'>
+              <Input type={'password'} placeholder={'Mật khẩu'} width={'100%'} height={'48px'} radius={'5px'} padding={'0 10px'} fontSize={'15px'} borderWidth={'1.5px'} changeBorderColor={true} borderColor={'border-gray-dark'} borderColorForcused={'border-white-primary'} borderColorError={'border-red-light'} />
+            </div>
+            <div className='mt-16'>
+              <Button width={'100%'} height={'48px'} radius={'60px'} backgroundColor={'bg-green-light'} label={'Đăng nhập'} fontColor={'text-black-primary'} fontSize={'15px'} fontStyle={'font-bold'} />
+            </div>
+          </form>
+
+          <div className='w-3/4 flex flex-col items-center my-10'>
+            <a className='underline font-bold hover:text-green-light text-[15px]' href="#">Quên mật khẩu của bạn?</a>
+            <div className='h-[1px] w-full bg-gray-dark my-10'></div>
+            <span className='block w-full text-gray-light text-[15px] text-center'>
+              Bạn chưa có tài khoản?
+              <a href="/register" className='ml-1 underline text-white-secondary hover:text-green-light font-bold'>Đăng ký Spotify.</a>
+            </span>
+          </div>
+
+        </div>
+      </div>
+
+      <div className="wrapper--footer w-full h-24 flex items-center justify-center pl-7">
+        <p className="footer--content text-center">
+          This site is protected by reCAPTCHA and the Google
+          <br />
+          <a href="https://policies.google.com/privacy" className="underline">Privacy Policy</a>
+          <span> and </span>
+          <a href="https://policies.google.com/terms" className="underline"> Terms of Service</a>
+          <span> apply.</span>
+        </p>
+      </div>
+    </div>
   )
 }
