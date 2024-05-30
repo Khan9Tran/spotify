ddb02dd48f6e9a3872e52e37860b0ed361bbdbd3
thanhhuy25 <nguyenhuythanh256@gmail.com>
2024-05-31 01:50:51 +0700

    Cập nhật font chữ của trang web.

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,28 +5,28 @@ module.exports = {
   ],
   theme: {
     extend: {
-        colors: {
-          'black-primary': '#000000',
-          'black-secondary': '#121212',
+      colors: {
+        'black-primary': '#000000',
+        'black-secondary': '#121212',
 
-          'white-primary': '#ffffff',
-          'white-secondary': '#f6f6f6',
+        'white-primary': '#ffffff',
+        'white-secondary': '#f6f6f6',
 
-          'green-light': '#1fdf64',
-          'green-dark': '#117a37',
+        'green-light': '#1fdf64',
+        'green-dark': '#117a37',
 
-          'gray-light': '#a7a7a7',
-          'gray-dark': '#727272',
+        'gray-light': '#a7a7a7',
+        'gray-dark': '#727272',
 
-          'red-light': '#e91429',
-          'red-dark': '#e35e60',
-        },
+        'red-light': '#e91429',
+        'red-dark': '#e35e60',
+      },
 
-        fontFamily: {
-          'bold': ['CircularStd-Bold', 'sans-serif'],
-          'light': ['CircularStd-Light', 'sans-serif'],
-          'medium': ['CircularStd-Medium', 'sans-serif'],
-        },
+      fontFamily: {
+        'bold': ['Gotham-Bold', 'sans-serif'],
+        'light': ['Gotham-Light', 'sans-serif'],
+        'medium': ['Gotham-Medium', 'sans-serif'],
+      },
     },
   },
   plugins: [],
