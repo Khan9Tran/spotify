b718b6132ac3cb1f62be8b01974d27f9a2a43db9
thanhhuy25 <nguyenhuythanh256@gmail.com>
2024-06-01 02:10:18 +0700

    Chỉnh sửa nội dung JSON server phản hồi client nhằm thông báo cho người dùng.

--- server/internal/auth/delivery/http/auth_handler.go
+++ server/internal/auth/delivery/http/auth_handler.go
@@ -1,7 +1,6 @@
 package authHttp
 
 import (
-	"fmt"
 	"net/http"
 	"spotify/internal/auth"
 	"spotify/internal/auth/presenter"
@@ -24,22 +23,36 @@ func (h *authHandler) Register() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		registerPayload := &presenter.RegisterPayload{}
 		if err := c.ShouldBindJSON(registerPayload); err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			c.JSON(http.StatusBadRequest, gin.H{
+				"status": "fail",
+				"error":  err.Error(),
+			})
 			return
 		}
 
 		if err := utils.Validate.Struct(registerPayload); err != nil {
 			errors := err.(validator.ValidationErrors)
-			c.JSON(http.StatusBadRequest, gin.H{"error": fmt.Errorf("invalid payload %v", errors).Error()})
+			_ = errors
+
+			c.JSON(http.StatusBadRequest, gin.H{
+				"status":  "fail",
+				"message": "Dữ liệu nhập chưa chính xác. Vui lòng kiểm tra lại!!!",
+			})
 			return
 		}
 
-		if err:= h.userCase.Register(&gin.Context{}, registerPayload.Email, registerPayload.Password, registerPayload.ConfirmPassword, registerPayload.Name); err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		if err := h.userCase.Register(&gin.Context{}, registerPayload.Email, registerPayload.Password, registerPayload.ConfirmPassword, registerPayload.Name); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"status":  "fail",
+				"message": "Email đã tồn tại",
+			})
 			return
 		}
-		
-		c.JSON(http.StatusOK, gin.H{"message": "success"})
+
+		c.JSON(http.StatusOK, gin.H{
+			"status":  "success",
+			"message": "Đăng ký thành công",
+		})
 	}
 }
 
@@ -48,26 +61,42 @@ func (h *authHandler) Login() gin.HandlerFunc {
 		loginPayload := &presenter.LoginPayload{}
 
 		if err := c.ShouldBindJSON(loginPayload); err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": "error when get JSON in request body"})
+			c.JSON(http.StatusBadRequest, gin.H{
+				"status": "fail",
+				"error":  err.Error(),
+			})
 			return
 		}
 		if err := utils.Validate.Struct(loginPayload); err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": "invalidate payload login"})
+			c.JSON(http.StatusBadRequest, gin.H{
+				"status":  "fail",
+				"message": "Dữ liệu nhập chưa chính xác. Vui lòng kiểm tra lại!!!",
+			})
 			return
+
 		}
 
 		token, err := h.userCase.Login(c.Request.Context(), loginPayload.Email, loginPayload.Password)
 
 		if err != nil {
 			if err == auth.ErrUserNotFound {
-				c.JSON(http.StatusBadRequest, gin.H{"error": "user not found"})
+				c.JSON(http.StatusBadRequest, gin.H{
+					"status":  "fail",
+					"message": "Tài khoản không tổn tại!!!",
+				})
 				return
 			}
 			if err == auth.ErrWrongPassword {
-				c.JSON(http.StatusBadRequest, gin.H{"error": "password is not correct"})
+				c.JSON(http.StatusBadRequest, gin.H{
+					"status":  "fail",
+					"message": "Email hoặc mật khẩu không chính xác!!!",
+				})
 				return
 			}
-			c.JSON(http.StatusInternalServerError, gin.H{"error": "internal server error"})
+			c.JSON(http.StatusBadRequest, gin.H{
+				"status":  "fail",
+				"message": "Đăng nhập thất bại!!!",
+			})
 			return
 		}
 		c.JSON(http.StatusOK, presenter.LoginResponse{Token: token})
