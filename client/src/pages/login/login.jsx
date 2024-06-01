fc74cf5e2ca1e15181bf607d086d252ca9928750
thanhhuy25 <nguyenhuythanh256@gmail.com>
2024-06-01 14:46:52 +0700

    Hoàn thành chức năng login (thông báo lỗi)

--- client/src/pages/login/login.jsx
+++ client/src/pages/login/login.jsx
@@ -148,7 +148,7 @@ export const Login = () => {
 
       <div className="wrapper--footer w-full h-24 flex items-center justify-center pl-7">
         <p className="footer--content text-center">
-          This site is protected by reCAPTCHA and the Google
+          This site is protected by reCAPTCHA and the Google 
           <a href="https://policies.google.com/privacy" className="underline">Privacy Policy</a>
           <span> and </span>
           <a href="https://policies.google.com/terms" className="underline"> Terms of Service</a>
