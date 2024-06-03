7bd8690dd88a1918f063cb585f6457dcf59a47c2
Khan9Tran <trankhangtctv@gmail.com>
2024-06-03 21:58:18 +0700

    fix grid

--- client/src/components/genreComponent.jsx
+++ client/src/components/genreComponent.jsx
@@ -1,6 +1,6 @@
 export const Genre = (item, index) => {
     return (
-        <div key ={index} className="w-1/4  h-auto bg-white m-2"  style={item.img ? {backgroundImage: `url(${item.img})`, borderRadius:'10px'} : {borderRadius:'10px'}}>
+        <div key ={index} className="w-[245px] min-h-[140px] bg-white my-3 mr-1"  style={item.img ? {backgroundImage: `url(${item.img})`, borderRadius:'10px'} : {borderRadius:'10px'}}>
             {item.name ? <span className="text-white-primary text-[14px] font-bold">{item.name}</span> : null}
         </div>
     );
--- client/src/components/genreList.jsx
+++ client/src/components/genreList.jsx
@@ -3,11 +3,11 @@ import BodyComponentTittle from "./bodyComponentTittle";
 export const GenreList = ({items}) => {
     return (
         <div
-        className=' max-w-full min-w-[150px] h-auto flex flex-col justify-start p-5 rounded-md hover:bg-[#313131] relative'>
+        className=' max-w-[1460px] min-w-[150px] flex flex-col justify-start p-5 rounded-md hover:bg-[#313131] relative'>
             <h1 className='text-white-primary text-[22px] font-bold my-2'>
             Duyệt tìm tất cả
             </h1>
-            <div className="flex flex-wrap">
+            <div className="flex flex-wrap justify-between">
                 {items.map((item, index) => (
                     Genre(item, index)
                 ))}
